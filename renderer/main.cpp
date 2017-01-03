558d99af89165677fdcda0b107f14c2b5aed83f1
Wenqi Zhang <zhangwenqi1988@gmail.com>
2017-01-03 00:15:27 -0800

    Set up CMake and adding a hello world.

---
+++ renderer/main.cpp
@@ -0,0 +1,9 @@
+/**
+ * Temporarily set for verification of environment setup purpose.
+ * Will be refactored soon.
+ */
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello World!";
+}
